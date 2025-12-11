56899421200f4adb26da43ae7e4e98f34b1808d6
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-12-11 09:33:44 +0100

    app icon

--- terminus/★ Scripts ★/Notes.gd
+++ terminus/★ Scripts ★/Notes.gd
@@ -8,20 +8,38 @@ func _on_button1_pressed() -> void:
 
 
 func _on_button2_pressed() -> void:
+	get_node("Panel/Bar/VBoxContainer2/Title").text = str(" ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/Date").text = str("02/Jan/2011      ")
 	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/ScrollContainer/RichTextLabel").text = str("Hello, this is two text… not sure what it's supposed to be.\n oh well. Some more text, yada yada yada. Testing the scroll function now! I have so much homework… Aaargh! help!")
 
 
 func _on_button3_pressed() -> void:
-	pass # Replace with function body.
+	get_node("Panel/Bar/VBoxContainer2/Title").text = str(" ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/Date").text = str("03/Jan/2011      ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/ScrollContainer/RichTextLabel").text = str("THREE! Hello, this is two text… not sure what it's supposed to be.\n oh well. Some more text, yada yada yada. Testing the scroll function now! I have so much homework… Aaargh! help!")
 
 
 func _on_button4_pressed() -> void:
-	pass # Replace with function body.
+	get_node("Panel/Bar/VBoxContainer2/Title").text = str(" ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/Date").text = str("04/Jan/2011      ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/ScrollContainer/RichTextLabel").text = str("FOUR! Hello, this is two text… not sure what it's supposed to be.\n oh well. Some more text, yada yada yada. Testing the scroll function now! I have so much homework… Aaargh! help!")
 
 
 func _on_button5_pressed() -> void:
-	pass # Replace with function body.
+	get_node("Panel/Bar/VBoxContainer2/Title").text = str(" ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/Date").text = str("05/Jan/2011      ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/ScrollContainer/RichTextLabel").text = str("FIVE! Hello, this is two text… not sure what it's supposed to be.\n oh well. Some more text, yada yada yada. Testing the scroll function now! I have so much homework… Aaargh! help!")
 
 
 func _on_button6_pressed() -> void:
-	pass # Replace with function body.
+	get_node("Panel/Bar/VBoxContainer2/Title").text = str(" ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/Date").text = str("06/Jan/2011      ")
+	get_node("Panel/Bar/VBoxContainer2/Panel/HBoxContainer/ScrollContainer/RichTextLabel").text = str("SIX! Hello, this is two text… not sure what it's supposed to be.\n oh well. Some more text, yada yada yada. Testing the scroll function now! I have so much homework… Aaargh! help!")
+
+
+func _on_NOTES_close_requested() -> void:
+	visible = false
+
+
+func _on_notes_pressed() -> void:
+	visible = true

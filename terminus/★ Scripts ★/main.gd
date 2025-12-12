8033d9c191f9020b149729ac4935da82ff27c765
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-12-12 12:33:47 +0100

    fixed some stuff

--- terminus/★ Scripts ★/main.gd
+++ terminus/★ Scripts ★/main.gd
@@ -1,4 +1,13 @@
 extends Control
-#time
+
 func _process(_delta):
+	#TIME!
 	$Bar/Time.text = Time.get_time_string_from_system()
+	
+	# DATE!
+	var unix_time = Time.get_unix_time_from_system()
+	var dt = Time.get_datetime_dict_from_unix_time(unix_time)
+
+	# Format: dd mm yyyy
+	var _text = "%02d %02d %04d" % [dt.day, dt.month, dt.year]
+	$Bar/Date.text = _text
